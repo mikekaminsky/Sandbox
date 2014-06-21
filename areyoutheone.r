5f877a239f71af63322be8584f0582bcb0cead28
Michael <ccUser@cc-notebook-19.home>
2014-06-21 01:03:17 -0400

    Working on decomposing the rules engine from the game player. Mostly have it working but am sometimes (?) returning null values. Haven't been able to track that down. Next need to add to the rules engine so that it 'freezes' columns when there is a change of 2

--- areyoutheone.r
+++ areyoutheone.r
@@ -10,7 +10,7 @@ n <- 10
 
 
 num_correct <- function(test, correct){
-    num <- sum(test == correct)
+    num <- sum(test[1:n] == correct)
     return(num)
 }
 
@@ -36,56 +36,53 @@ new_draw <- function(h){
     #return(rules_pos)
 #}
 
-rules_engine <- function(vec,h,c){
+rules_engine <- function(vec,h){
     repeat{
         r <- sample(1:n,2,FALSE) 
         r1 <- r[1]
         r2 <- r[2]
-        prior <- num_correct(vec,c)
-        rules_pos <- swap(vec,r1,r2)
-        post <- num_correct(rules_pos,c)
-        if (tail(duplicated(rbind(h,rules_pos)),1)==FALSE){
-            h <- rbind(h,rules_pos)
-            if(prior >= post){
-                rules_pos <- vec
-            }
-            break
+        if (length(h) == n+1) {
+            best <- h[1:n]
+        }else{
+           best <- h[max(h[,n+1])==h[,n+1],]
+           if (length(best)>n+1){
+                best <- head(best,1)
+           }
+           best <- best[1:n]
         }
-
+        rules_pos <- swap(best,r1,r2)
+        if (length(h) != n+1) {
+            if (tail(duplicated(rbind(h[,1:n],rules_pos)),1)==FALSE){
+                break
+            }
+        }else{break}
     }
-    return(list(rules_pos,h))
-}
-
-get_num_correct <- function(his_frame,c){
-    output <- apply(his_frame,1,function(x) num_correct(x,c))
-    output <- cbind(his_frame,output)
-    return(output)
+    return(rules_pos)
 }
 
 play_game <- function(){
     cor <- sample(1:n,n,FALSE)
     new_pos <- c(1:n)    
-    history <- new_pos
+    history <- c(new_pos,num_correct(new_pos,cor))
     i=1
     while (num_correct(new_pos,cor) != n){
-        #new_pos <- new_draw(history)
-        both <- rules_engine(new_pos,history,cor)
-        new_pos <- both[[1]]
-        history <- both[[2]]
+        new_pos  <- rules_engine(new_pos,history)
+        new_pos <- c(new_pos,num_correct(new_pos,cor))
+        history <- rbind(history,new_pos)
         i = i+1
    } 
-   #print(get_num_correct(history,cor))
-   return(i)
+   return(history)
 }
 
 repeat_game <- function(games){
-    
     x <- c(1:games) 
     for(j in 1:games){
-        x[j] <- play_game()
+        num_tries <- nrow(play_game())
+        x[j] <- num_tries
     }
     return(x)
 }
 
-game_output <- repeat_game(100)
-
+#game_output <- repeat_game(100)
+#h1 <- play_game()
+print(nrow(play_game()))
